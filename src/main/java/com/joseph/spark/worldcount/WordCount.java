eef5ccf9507054eb4c74f7ad417fad5ec8862d3e
JosephChan007 <zjzyp007@hotmail.com>
2020-05-26 23:36:11 +0800

    commit

--- src/main/java/com/joseph/spark/worldcount/WordCount.java
+++ src/main/java/com/joseph/spark/worldcount/WordCount.java
@@ -12,7 +12,7 @@ import scala.Tuple2;
 import java.util.Arrays;
 import java.util.Iterator;
 
-public class WorldCount {
+public class WordCount {
 
 
     public static void main(String[] args) {
---
+++ src/main/java/com/joseph/spark/worldcount/WordCountLumda.java
@@ -0,0 +1,35 @@
+package com.joseph.spark.worldcount;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+
+public class WordCountLumda {
+
+    public static void main(String[] args) {
+
+        String sourceFile = "hdfs://hdfs-host3/spark/study/wc-source.txt";
+        String targetFile = "hdfs://hdfs-host3/spark/study/wc-target";
+
+        SparkConf conf = new SparkConf().setAppName("WordCountLumda");
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+
+        JavaRDD<String> lines = jsc.textFile(sourceFile);
+        JavaRDD<String> words = lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
+        JavaPairRDD<String, Integer> wordAndOne = words.mapToPair(word -> new Tuple2<>(word, 1));
+        JavaPairRDD<String, Integer> reduced = wordAndOne.reduceByKey((m, n) -> m + n);
+        JavaPairRDD<Integer, String> swaped = reduced.mapToPair(tp -> tp.swap());
+        JavaPairRDD<Integer, String> sorted = swaped.sortByKey(false);
+        JavaPairRDD<String, Integer> result = sorted.mapToPair(tp -> tp.swap());
+        result.saveAsTextFile(targetFile);
+
+        jsc.stop();
+    }
+
+
+
+}
