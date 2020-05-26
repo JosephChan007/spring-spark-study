e26cb7dba5d1b37fec4253ce8a27d5a2100d0e56
JosephChan007 <zjzyp007@hotmail.com>
2020-05-26 22:03:34 +0800

    Initial commit

---
+++ src/main/java/com/joseph/spark/SpringSparkStudyApplication.java
@@ -0,0 +1,13 @@
+package com.joseph.spark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSparkStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSparkStudyApplication.class, args);
+    }
+
+}
